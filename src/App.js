bf38bed282f3848da0bfef8a65d058180ca9260f
Saliou1920 <diopsaliou45@gmail.com>
2021-09-11 22:45:36 -0300

    Changing the title name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./assets/css/style.css"
 import Images from './components/Images';
 
 function App() {
 
+    useEffect(() => {
+      document.title = "Gallery";
+    }, []);
     return (
         <section className="flex justify-center">
             <div className="max-w-7xl">
