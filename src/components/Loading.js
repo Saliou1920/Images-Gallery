b0fcc46f6b5b7be008aa5d1694db9b76d92c434a
Saliou1920 <diopsaliou45@gmail.com>
2021-10-08 15:36:31 -0300

    changing loader image

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { Spinner } from 'react-bootstrap';
+import Loader from "react-loader-spinner";
+import React from 'react';
 
 export default function Loading() {
     return (
         <div className="flex h-screen">
             <div className="m-auto">
-                {<Spinner animation="border" variant="danger" />}
+                <Loader
+                type="ThreeDots"
+                color="##151515"
+                height={100}
+                width={100}
+                timeout={3000} //2 secs
+                />
             </div>
         </div>
+        
     )
 }
