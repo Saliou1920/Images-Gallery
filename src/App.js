a9b33219bc21ca8bb905a46a1076acce44844db5
Saliou1920 <diopsaliou45@gmail.com>
2021-10-08 19:29:14 -0300

    adding Login and Gallery to routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./assets/css/style.css"
-import Gallery from './page/Gallery';
-import Home from './page/Home';
-import Login from './page/Login';
 import routes from './utils/routes';
 
 function App() {
@@ -18,17 +15,9 @@ function App() {
                             path={route.path} 
                             exact={route.exact} 
                             component={route.component}
-
                         />
                     ))
-                }
-                <Route path="/login">
-                    <Login/>
-                </Route>
-                <Route path="/gallery">
-                    <Gallery/>
-                </Route>
-                
+                }                
             </Switch>
         </Router>
     );
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,10 +1,22 @@
+import Gallery from "../page/Gallery";
 import Home from "../page/Home";
+import Login from "../page/Login";
 
 const routes =  [
     {
         path: '/',
         exact: true,
         component: () => <Home/>
+    },
+    {
+        path: '/login',
+        exact: true,
+        component: () => <Login/>
+    },
+    {
+        path: '/gallery',
+        exact: true,
+        component: () => <Gallery/>
     }
 ]
 
