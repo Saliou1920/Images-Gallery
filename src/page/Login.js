dc0d0a7cd0edfaec677f930088628d2b1fdceb28
Saliou1920 <diopsaliou45@gmail.com>
2021-10-09 12:36:28 -0300

    login form

--- src/page/Login.js
+++ src/page/Login.js
@@ -2,8 +2,38 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div className="flex h-screen">
-        <h1 className="m-auto text-3xl">Login Page</h1>
+        <div className="flex h-screen bg-gray-200">
+        <div className="m-auto w-1/3 text-white flex flex-wrap
+                        justify-center shadow-lg bg-indigo-900">
+            <form className="m-5 w-10/12">
+                <h1 className="w-full text-4xl text-center my-6 tracking-widest">
+                    Login
+                </h1>
+                <div className="w-full my-6">
+                    <input 
+                        type="email"
+                        className="p-2 rounded shadow w-full"
+                        placeholder="Email or Username"
+                    />
+                </div>
+
+                <div>
+                    <input 
+                        type="password"
+                        className="p-2 rounded shadow w-full"
+                        placeholder="Password"
+                    />
+                </div>
+                <div className="w-full my-10">
+                    <button 
+                        type="submit"
+                        className="p-2 rounded shadow w-full bg-yellow-400 text-black"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
     );
     
