41227ab9fdf8a5f5bad0d93fdd6f67708d8b8c42
Saliou1920 <diopsaliou45@gmail.com>
2021-10-10 23:21:02 -0300

    testing login form

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
 export default function Login() {
+    function handleForm(e) {
+        e.preventDefault();
+        console.log("Submitted");
+    }
     return (
         <div className="flex h-screen bg-gray-200">
         <div className="m-auto w-1/3 text-white flex flex-wrap
                         justify-center shadow-lg bg-indigo-900">
-            <form className="m-5 w-10/12">
+            <form className="m-5 w-10/12" onSubmit={handleForm}>
                 <h1 className="w-full text-4xl text-center my-6 tracking-widest">
                     Login
                 </h1>
                 <div className="w-full my-6">
                     <input 
                         type="email"
-                        className="p-2 rounded shadow w-full"
+                        className="p-2 rounded shadow w-full text-black"
                         placeholder="Email or Username"
                     />
                 </div>
@@ -20,7 +24,7 @@ export default function Login() {
                 <div>
                     <input 
                         type="password"
-                        className="p-2 rounded shadow w-full"
+                        className="p-2 rounded shadow w-full text-black"
                         placeholder="Password"
                     />
                 </div>
