f1ff7a493285dfa5a6f315d8f277f8efdd6a5f09
Saliou1920 <diopsaliou45@gmail.com>
2021-10-20 17:47:30 -0300

    firebase configuration

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnkNwVdznHv6yqETCuGucIQc9ua1D_vE4",
+  authDomain: "image-gallery-18ddf.firebaseapp.com",
+  projectId: "image-gallery-18ddf",
+  storageBucket: "image-gallery-18ddf.appspot.com",
+  messagingSenderId: "399626336611",
+  appId: "1:399626336611:web:d2d28c20ebf15247383b56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
