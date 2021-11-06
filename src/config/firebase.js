2bbd6c2fd5138c6f6cc90ac9736f9d93861abfa0
Saliou1920 <diopsaliou45@gmail.com>
2021-11-05 21:06:46 -0300

    configure firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,10 +11,10 @@ const firebaseConfig = {
   projectId: "image-gallery-18ddf",
   storageBucket: "image-gallery-18ddf.appspot.com",
   messagingSenderId: "399626336611",
-  appId: "1:399626336611:web:d2d28c20ebf15247383b56"
+  appId: "1:399626336611:web:d2d28c20ebf15247383b56",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
+const auth = getAuth(app);
+export default auth;
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,58 +1,56 @@
-import React from 'react'
-import app from '../config/firebase';
+import React from "react";
+import app from "../config/firebase";
 import { getAuth, signInWithEmailAndPassword } from "../config/firebase/auth";
 export default function Login() {
-    function handleForm(e) {
-        e.preventDefault();
-        
+  function handleForm(e) {
+    e.preventDefault();
 
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, "saliou.gmail.com", "Saliou")
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            // ...
-            console.log(user);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
-    }
-    return (
-        <div className="flex h-screen bg-gray-200">
-        <div className="m-auto w-1/3 text-white flex flex-wrap
-                        justify-center shadow-lg bg-indigo-900">
-            <form className="m-5 w-10/12" onSubmit={handleForm}>
-                <h1 className="w-full text-4xl text-center my-6 tracking-widest">
-                    Login
-                </h1>
-                <div className="w-full my-6">
-                    <input 
-                        type="email"
-                        className="p-2 rounded shadow w-full text-black"
-                        placeholder="Email or Username"
-                    />
-                </div>
+    const auth = getAuth();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    signInWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log("success");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  return (
+    <div className="flex h-screen bg-gray-200">
+      <div
+        className="m-auto w-1/3 text-white flex flex-wrap
+                        justify-center shadow-lg bg-indigo-900"
+      >
+        <form className="m-5 w-10/12" onSubmit={handleForm}>
+          <h1 className="w-full text-4xl text-center my-6 tracking-widest">
+            Login
+          </h1>
+          <div className="w-full my-6">
+            <input
+              type="email"
+              className="p-2 rounded shadow w-full text-black"
+              placeholder="Email or Username"
+            />
+          </div>
 
-                <div>
-                    <input 
-                        type="password"
-                        className="p-2 rounded shadow w-full text-black"
-                        placeholder="Password"
-                    />
-                </div>
-                <div className="w-full my-10">
-                    <button 
-                        type="submit"
-                        className="p-2 rounded shadow w-full bg-yellow-400 text-black"
-                    >
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </div>
+          <div>
+            <input
+              type="password"
+              className="p-2 rounded shadow w-full text-black"
+              placeholder="Password"
+            />
+          </div>
+          <div className="w-full my-10">
+            <button
+              type="submit"
+              className="p-2 rounded shadow w-full bg-yellow-400 text-black"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    );
-    
+  );
 }
