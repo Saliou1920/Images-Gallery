2ae2f520e7319d5110743db4e3e8c589a9fc8494
Saliou1920 <diopsaliou45@gmail.com>
2021-11-09 00:47:10 -0400

    adding animation

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <h1 className="m-auto text-3xl">Welcome Home</h1>
+      <motion.h1
+        animate={{ scale: 2 }}
+        transition={{ duration: 0.5 }}
+        className="m-auto text-4xl font-bold text-center"
+      >
+        Welcome to the Home Page
+      </motion.h1>
     </div>
   );
 }
